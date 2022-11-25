9e5136898303e1cd8494883d42cd2012bea4ae2a
kkyu0718 <kyuwon0718@gmail.com>
2022-11-25 14:51:18 +0900

    feat: seminar4 미들웨어 추가

--- seminar4/basic/src/service/userService.ts
+++ seminar4/basic/src/service/userService.ts
@@ -1,17 +1,49 @@
+import { UserCreateDTO } from './../interfacces/UserCreateDto';
 import { PrismaClient } from "@prisma/client";
+import bcrypt from 'bcryptjs';
+import { sc } from '../constants';
+import { UserSignInDTO } from '../interfacces/UserSignInDto';
+
 const prisma = new PrismaClient();
 
 //* 유저 생성
-const createUser = async (name: string, email: string, age: number) => {
+const createUser = async (userCreateDto: UserCreateDTO) => {
+  //? 넘겨받은 password를 bcrypt의 도움을 받아 암호화
+  const salt = await bcrypt.genSalt(10); //^ 매우 작은 임의의 랜덤 텍스트 salt
+  const password = await bcrypt.hash(userCreateDto.password, salt); //^ 위에서 랜덤을 생성한 salt를 이용해 암호화
 
-  return await prisma.user.create({
+  const data = await prisma.user.create({
     data: {
-      userName: name, 
-      age,
-      email
-    }
+      userName: userCreateDto?.name,
+      age: userCreateDto?.age,
+      email: userCreateDto.email,
+      password,
+    },
   });
-  
+
+  return data;
+};
+
+//* 로그인
+const signInUser = async (userSignInDto: UserSignInDTO) => {
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        email: userSignInDto.email,
+      },
+    });
+    if (!user) return null;
+
+    //? bcrypt가 DB에 저장된 기존 password와 넘겨 받은 password를 대조하고,
+    //? match false시 401을 리턴
+    const isMatch = await bcrypt.compare(userSignInDto.password, user.password);
+    if (!isMatch) return sc.UNAUTHORIZED;
+
+    return user.id;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
 
 //* 유저 전체 조회
@@ -59,6 +91,7 @@ const userService = {
   getAllUser,
   updateUser,
   deleteUser,
+  signInUser
 };
 
 export default userService;
